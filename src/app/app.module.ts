19819a262563423dd1410a7eb20040f07fa33fd6
José Antunes <joseantunes@eqs.local>
2017-06-09 18:01:05 +0100

    project mount

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import { MyComponent2.component.tsComponent } from './my-component2.component.ts/my-component2.component.ts.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent,
-    MyComponent2.component.tsComponent
-],
+  declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
